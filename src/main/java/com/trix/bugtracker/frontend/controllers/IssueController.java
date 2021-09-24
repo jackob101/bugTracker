e96dabf27d668a52098bf1fbaeded86b18f7b20f
Jackob <pietrzyk.jakub001@gmail.com>
2021-09-24 16:36:08 +0200

    Small test

--- src/main/java/com/trix/bugtracker/frontend/controllers/IssueController.java
+++ src/main/java/com/trix/bugtracker/frontend/controllers/IssueController.java
@@ -119,5 +119,10 @@ public class IssueController {
 	return issueService.changePriority(issueId, priority);
     }
 
+    @GetMapping(path = "test")
+    public String getTest(){
+        return "Yay CI/CD works!!!!";
+    }
+
 
 }
